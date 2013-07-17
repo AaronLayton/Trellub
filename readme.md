933511fcd8cf83218052e2f143ffc0e70d899ad6
Aaron <aaron.r.layton@gmail.com>
2013-07-17 01:22:30 +0100

    Detecting Trello or Github Dynamically loading Trellub HTML Adding button for Trello Made a start for detecting Github

--- readme.md
+++ readme.md
@@ -1,4 +1,17 @@
-# Trellub
+Trellub
+===========
 
+A neat Chrome extension that provides extra functionality to Trello and Github for managing Tasks and Issues
 
-Create github issues from trello cards.
+## Installation
+
+### Chrome Web Store
+
+* [todo] - Aaron
+
+### Manual
+1. Clone the repo.
+2. Open chrome and go to `chrome://extensions`
+3. Make sure Developer Mode is checked.
+4. Click "Load unpacked extension..."
+5. Select the `trellub` directory inside the repo.
---
+++ trellub/main.js
@@ -0,0 +1,51 @@
+var trellub = (function($, Trello) {
+	
+	var version = "0.0.1",
+
+	addGithubButton = function(){
+		var githubButton = $($('#gituhub-button-template').html());
+
+		githubButton.insertBefore($('div.other-actions > div > *:nth-child(1)'));
+	},
+
+	loadTrellubHtml = function(){
+		$('body').append('<div id="trellub-container"></div>');
+		$("#trellub-container").load(chrome.extension.getURL('trellub.html'), function(){
+			// Load complete
+			console.info("Trello page loaded in the bg");
+		});
+	},
+
+	setupTrello = function(){
+		// Monitor the task window popup for changes so we can add out button
+		ob = new MutationObserver(function(objs, observer){
+		  var wasHidden = $.inArray('visibility: hidden', $.map(objs, function(o){return o['oldValue'];}).join().split(/;,? ?/)) != -1;
+		  if($('.window').is(':visible') && wasHidden) {
+		    trellub.addGithubButton();
+		  }
+		});
+
+		//trigger ob's callback every time the style attribute on window changes
+		ob.observe($('.window')[0], {attributes:true, attributeFilter:['style'], attributeOldValue:true})
+	},
+
+	setupGithub = function(){
+		console.log("Hi there Github");
+	};
+
+
+
+	loadTrellubHtml();
+	
+
+	if (window.location.href.indexOf("trello") > -1)
+		setupTrello();
+	else
+		setupGithub();
+
+	return {
+		addGithubButton: addGithubButton
+	};
+})(jQuery, Trello);
+
+
